fdec57652d03d68db84852d993b97f089ce27eeb
Nick Bogdanov <bezumnoe@gmail.com>
2010-03-27 00:20:26 +0300

    Log message fancy formatted

--- inc/classes/entities/user_complete.class.php
+++ inc/classes/entities/user_complete.class.php
@@ -106,6 +106,7 @@ class UserComplete extends EntityBase {
 				// If match found and sessionAddress is not empty
 				if (!$this->IsEmpty() && $sessionAddress) {
 					// .. updating session address
+		 			SaveLog("Пользователь <strong>".$this->User->Login."</strong>:<ul class=\"ListChanges\"><li><b>Адрес сессии:</b> ".$this->User->SessionAddress." <b>&rarr;</b> ".$sessionAddress."</li></ul>", -1, "", AdminComment::SEVERITY_WARNING);
 					$this->User->UpdateSessionAddress($sessionAddress);
 				}
 			}
