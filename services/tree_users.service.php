f07fe4e334073d52435413c111c87b9559d2ff16
Nick Bogdanov <bezumnoe@gmail.com>
2010-05-13 00:02:19 +0400

    Tree drawing basic functionality added

---
+++ services/tree_users.service.php
@@ -0,0 +1,27 @@
+<?
+
+	require_once "base.service.php";
+
+	$t = new TreeNode();
+	$result = "";
+	
+	$q = $t->GetTreeUsers();
+	for ($i = 0; $i < $q->NumRows(); $i++) {
+		$q->NextResult();
+		$result .= ($i > 0 ? "," : "").$t->UserInfoToJs($q);
+	}
+	$q->Release();
+
+	echo "u=[".$result."];";
+	$result = "";
+
+	$q = $t->GetByCondition();
+	for ($i = 0; $i < $q->NumRows(); $i++) {
+		$q->NextResult();
+		$t->FillFromResult($q);
+		$result .= ($i > 0 ? "," : "").$t->ToJs();
+	}
+	$q->Release();
+
+	echo "r=[".$result."];";
+?>
