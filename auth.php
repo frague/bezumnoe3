394f5ac009e3981d41c847e82fd4d50e5a161d7d
Nick Bogdanov <bezumnoe@gmail.com>
2011-01-24 23:40:24 +0300

    OpenID authorization + some UI form updates

---
+++ auth.php
@@ -0,0 +1,61 @@
+<?php
+
+	$root = "./";
+	require_once $root."server_references.php";
+	require_once $root."inc/classes/openid.class.php";
+
+	$login = $_POST[LOGIN_KEY];
+	$provider_id = round($_POST[OPENID_KEY]);
+
+	if ($_POST['openid_action'] == "login") { // Get identity from user and redirect browser to OpenID Server
+
+		if (!$login || !$provider_id)  {
+			exit;
+		}	
+
+		$op = new OpenIdProvider();
+		$op->GetById($provider_id);
+		if ($op->IsEmpty()) {
+			exit;
+		}
+
+		$referer = $_POST[REFERER_KEY] ? $_POST[REFERER_KEY] : $_SERVER["HTTP_REFERER"];
+    	$openid_url = $op->MakeUrl($login);
+    
+	    $openid = new SimpleOpenID;
+	    $openid->SetIdentity($openid_url);
+	    $openid->SetTrustRoot('http://' . $_SERVER["HTTP_HOST"]);
+	    $openid->SetRequiredFields(array('email','fullname'));
+	    $openid->SetOptionalFields(array('dob','gender','postcode','country','language','timezone'));
+	    if ($openid->GetOpenIDServer()) {
+	        $openid->SetApprovedURL('http://'.$_SERVER["HTTP_HOST"]."/auth.php?back=".urlencode($referer));      // Send Response from OpenID server to this script
+	        $openid->Redirect();     // This will redirect user to OpenID Server
+	    } else {
+	        $error = $openid->GetError();
+	        echo "ERROR CODE: ".$error['code']."<br>";
+	        echo "ERROR DESCRIPTION: ".$error['description']."<br>";
+	    }
+	    exit;
+	} else if ($_GET['openid_mode'] == 'id_res') {     // Perform HTTP Request to OpenID server to validate key
+
+	    $openid = new SimpleOpenID;
+	    $openid->SetIdentity($_GET['openid_identity']);
+	    $openid_validation_result = $openid->ValidateWithServer();
+
+	    if ($openid_validation_result == true) {         // OK HERE KEY IS VALID
+	        echo "Confirmed.";
+	    } else if ($openid->IsError() == true) {            // ON THE WAY, WE GOT SOME ERROR
+	        $error = $openid->GetError();
+	        echo "ERROR CODE: ".$error['code']."<br>";
+	        echo "ERROR DESCRIPTION: ".$error['description']."<br>";
+	    } else {                                            // Signature Verification Failed
+	        echo "Invalid Authorization";
+	    }
+	} else if ($_GET['openid_mode'] == 'cancel') { // User Canceled your Request
+	    echo "User canceled request";
+	} 
+
+
+
+
+?>
