9b9cb7a844e4d0b74ffdc6ae1054013e4449fe64
Nick Bogdanov <bezumnoe@gmail.com>
2011-01-24 15:40:19 +0300

    Library added. Post form prepared for OpenID data

--- inc/ui_parts/post_form.php
+++ inc/ui_parts/post_form.php
@@ -16,17 +16,21 @@
 			<input type="radio" name="AUTH" id="AUTH_LOGGED" value="0" checked="checked" /> <label for="AUTH_LOGGED">активный пользователь (<strong id="LoggedLogin"><?php echo $user->User->Login ?></strong>)</label><br />
 		</span>
 		<input type="radio" name="AUTH" id="AUTH_NOW" value="1" /> <label for="AUTH_NOW">по логину и паролю</label>
-
 		<div id="AuthByLogin">
-			логин: <input name="<?php echo LOGIN_KEY ?>" id="<?php echo LOGIN_KEY ?>" size="20" />, 
-			пароль: <input type="password" name="<?php echo PASSWORD_KEY ?>" id="<?php echo PASSWORD_KEY ?>" size="20" />
-			
+			логин: <input name="<?php echo LOGIN_KEY ?>" id="<?php echo LOGIN_KEY ?>" size="20" tabindex="1" />, 
+			пароль: <input type="password" name="<?php echo PASSWORD_KEY ?>" id="<?php echo PASSWORD_KEY ?>" size="20" tabindex="2" />
+		</div>
+
+		<input type="radio" name="AUTH" id="AUTH_OPENID" value="2" disabled /> <label for="AUTH_OPENID">с использованием OpenID</label>
+		<div id="AuthByOpenID">
+			логин: <input name="<?php echo LOGIN_KEY ?>" id="<?php echo LOGIN_KEY ?>" size="20" tabindex="1" disabled />, 
+			сервис: ...
 		</div>
 
 		<h4>Заголовок:</h4>
-		<input id="TITLE" name="TITLE" class="Wide" size="30" tabindex="1" /><br>
+		<input id="TITLE" name="TITLE" class="Wide" size="30" tabindex="3" /><br>
 		<h4 class="Mandatory">Текст сообщения:</h4>
-		<textarea id="CONTENT" name="CONTENT" class="Wide" cols="30" rows="10" tabindex="2">
+		<textarea id="CONTENT" name="CONTENT" class="Wide" cols="30" rows="10" tabindex="4">
 		</textarea><br>
 		<div class="Right">	
 			<div style="float:left" id="IsProtected">
@@ -34,7 +38,10 @@
 			</div>
 			<input type="Button" value="Цитата" onclick="MakeCite()" id="buttonCite">
 			<input type="Button" value="Отменить" onclick="CancelReply()" style="margin-right:20px">
-			<input type="Button" value="Отправить" onclick="AddMessage(this)" id="SubmitMessageButton" tabindex="3" style="font-weight:bold">
+			<input type="Button" value="Отправить" onclick="AddMessage(this)" id="SubmitMessageButton" tabindex="5" style="font-weight:bold">
 		</div><?php } ?>
 	</div>
 </div>
+
+<script language="javascript">OpenReplyForm();</script>
+	
