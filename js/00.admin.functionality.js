de3f0d2a943f794d68974f9f874f8f9aba5e8255
Nick Bogdanov <bezumnoe@gmail.com>
2011-02-19 00:08:48 +0300

    Users settings and admin options UI

--- js/00.admin.functionality.js
+++ js/00.admin.functionality.js
@@ -1,4 +1,4 @@
-//2.6
+//2.7
 /*
 	Admin only functionality.
 	Will be loaded only if server rights checking is == adminRights.
@@ -12,22 +12,33 @@ function CreateAdminTab() {
 
 /* Usermanager admins' section */
 
-function umExtraButtons(id, login, obj) {
- 	var td = MakeSection("Опции администратора:");
- 	var ul = d.createElement("ul");
+function umExtraButtons(tr, id, login, obj) {
+ 	var td1 = MakeSection("Опции администратора:");
+ 	var ul1 = d.createElement("ul");
 
-	ul.appendChild(MakeUserMenuLink(MakeButtonLink("ShowUser(" + id + ",'" + login + "')", "Профиль", obj, "")));
-	ul.appendChild(MakeUserMenuLink(MakeButtonLink("DeleteUser(" + id + ",'" + login + "', this)", "Удалить", obj, "Red")));
+	ul1.appendChild(MakeUserMenuLink(MakeButtonLink("ShowUser(" + id + ",'" + login + "')", "Профиль", obj, "")));
 	if (me.IsSuperAdmin()) {
-		umAdditionalExtraButtons(ul, id, login, obj);
+		umAdditionalExtraButtons(ul1, id, login, obj);
 	}
-	td.appendChild(ul);
-	return td;
+	td1.appendChild(ul1);
+	tr.appendChild(td1);
+
+ 	var td2 = MakeSection("Операции:", "Red");
+ 	var ul2 = d.createElement("ul");
+	ul2.appendChild(MakeUserMenuLink(MakeButtonLink("DeleteUser(" + id + ",'" + login + "', this)", "Удалить", obj, "Red")));
+	td2.appendChild(ul2);
+	tr.appendChild(td2);
+};
+
+/* Custom Tabs creation */
+
+function CustomTab(id, name, o, tab_prefix, name_prefix) {
+	var tab_id = tab_prefix + id;
+	CreateUserTab(id, name, new o(), name_prefix, "", tab_id);
 };
 
 function ShowUser(id, name) {
-	var tab_id = "u" + id;
-	CreateUserTab(id, name, new Profile(), "", "", tab_id);
+	CustomTab(id, name, Profile, "u", "");
 };
 
 function DeleteUser(id, name, a) {
@@ -38,9 +49,7 @@ function DeleteUser(id, name, a) {
 function DeleteUserConfirmed(id, obj) {
 	var req = new Requestor(servicesPath + "user_delete.service.php", obj);
 	req.Callback = RefreshList;
-//	req.Callback = function() {ReRequestData(obj)};
 	req.Request(["user_id"], [id]);
-//	obj.Inputs["BY_NAME"].DelayedRequestor.Request();
 	obj.Inputs["FILTER_BANNED"].DelayedRequestor.Request();
 };
 
