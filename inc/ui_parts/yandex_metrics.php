df5039d05b9c654860a83c2aef92f6c6758f0f54
Nick S. Bogdanov (from office) <bezumnoe@gmail.com>
2011-12-30 13:27:45 +0400

    yandex_metrics statistics added

---
+++ inc/ui_parts/yandex_metrics.php
@@ -0,0 +1,14 @@
+<!-- Yandex.Metrika counter -->
+<div style="display:none;"><script type="text/javascript">
+(function(w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter11382805 = new Ya.Metrika({id:11382805, enableAll: true, ut:"noindex", webvisor:true});
+        }
+        catch(e) { }
+    });
+})(window, "yandex_metrika_callbacks");
+</script></div>
+<script src="//mc.yandex.ru/metrika/watch.js" type="text/javascript" defer="defer"></script>
+<noscript><div><img src="//mc.yandex.ru/watch/11382805?ut=noindex" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
