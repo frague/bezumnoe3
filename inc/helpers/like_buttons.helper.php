ffa55cceb3c12d59d8e3b56df73db72ce3fff699
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2014-06-24 16:30:32 +0400

    Adds more proper meta information to the blog pages to be reflected properly by social services when shared

--- inc/helpers/like_buttons.helper.php
+++ inc/helpers/like_buttons.helper.php
@@ -1,54 +1,54 @@
 <?php
-	require_once $root."inc/classes/like_buttons.class.php";
+    require_once $root."inc/classes/like_buttons.class.php";
 
-	function GetHeadIncludes() {
-		$result = "";
-		foreach (array(VkLikeButton, TwitterLikeButton, GooglePlusButton, FacebookLikeButton) as $b) {
-			$button = new $b();
-			$result .= $button->getHeadContent()."\n";
-		}
-		return $result;
-	}
+    function GetHeadIncludes() {
+        $result = "";
+        foreach (array(VkLikeButton, TwitterLikeButton, GooglePlusButton, FacebookLikeButton) as $b) {
+            $button = new $b();
+            $result .= $button->getHeadContent()."\n";
+        }
+        return $result;
+    }
 
-	function MakeDescription($record) {
-		$DESCRIPTION_LENGTH = 255;
-		$strip_expression = "/[. \t()\n\r][^. \t()\n\r]*$/";
+    function MakeDescription($record) {
+        $DESCRIPTION_LENGTH = 155;
+        $strip_expression = "/[. \t()\n\r][^. \t()\n\r]*$/";
 
-		$result = substr(strip_tags($record->Content), 0, $DESCRIPTION_LENGTH);
-		$result = preg_replace($strip_expression, "", $result);
+        $result = substr(strip_tags($record->Content), 0, $DESCRIPTION_LENGTH);
+        $result = preg_replace($strip_expression, "", $result);
         $result = preg_replace("/##CUTLINK=([^#]+)##/", "\\1", $result);
         $result = preg_replace("/##([A-Z]+)##/", "", $result);
 
-//		$result = $record->Title."\\n".$result;
-		return $result;
-	}
-	
-	function FillButtonObjects($title = "", $description = "", $url = "", $image = "") {
-		$result = array();
-		foreach (array(VkLikeButton, TwitterLikeButton, GooglePlusButton, FacebookLikeButton) as $b) {
-			array_push($result, new $b($title, $description, $url, $image));
+//      $result = $record->Title."\\n".$result;
+        return $result;
+    }
+    
+    function FillButtonObjects($title = "", $description = "", $url = "", $image = "", $site_name = "", $tags = "") {
+        $result = array();
+        foreach (array(VkLikeButton, TwitterLikeButton, GooglePlusButton, FacebookLikeButton) as $b) {
+            array_push($result, new $b($title, $description, $url, $image, $site_name, $tags));
 
-		}
-		return $result;
-	}
-	
-	function GetMetadata($buttons = array()) {
-		$result = "";
-		foreach ($buttons as $b) {
-			if (!$result) {
-				$result .= $b->getBasicMetadata()."\n";
-			}
-			$result .= $b->getMetadata()."\n";
-		}
-		return $result;
-	}
+        }
+        return $result;
+    }
+    
+    function GetMetadata($buttons = array()) {
+        $result = "";
+        foreach ($buttons as $b) {
+            if (!$result) {
+                $result .= $b->getBasicMetadata()."\n";
+            }
+            $result .= $b->getMetadata()."\n";
+        }
+        return $result;
+    }
 
-	function GetButtonsMarkup($buttons = array()) {
-		$result = "<table class=\"Likes\"><tr>\n";
-		foreach ($buttons as $b) {
-			$result .= "<td>".$b->getButtonContent()."</td>\n";
-		}
-		return $result."</tr></table>";
-	}
+    function GetButtonsMarkup($buttons = array()) {
+        $result = "<table class=\"Likes\"><tr>\n";
+        foreach ($buttons as $b) {
+            $result .= "<td>".$b->getButtonContent()."</td>\n";
+        }
+        return $result."</tr></table>";
+    }
 
 ?>
