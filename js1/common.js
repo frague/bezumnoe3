e8799f1c63d0e1b104d6fdd47c5ea95fb101d290
Nikolay Bogdanov <nbogdanov@Nick-Bogdanovs-notebook.local>
2016-02-17 09:35:29 +0300

    Optimizes links to users profiles

--- js1/common.js
+++ js1/common.js
@@ -3,8 +3,13 @@ function Feedback() {
 };
 
 var infoPopUp;
-function Info(id) {
-    infoPopUp = open("/user/" + id + ".html", "info", "width=550,height=600,toolbar=0,location=0,directories=0,status=1,menubar=0,resizable=1");
+function info(id) {
+    infoPopUp = open(
+        "/user/" + id + ".html",
+        "info",
+        "width=550,height=600,toolbar=0,location=0,directories=0,status=1,menubar=0,resizable=1"
+    );
+    return false;
 }
 
 /* Letterizing */
