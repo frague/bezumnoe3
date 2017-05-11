98e18d923585869c29dbb20e8873c86754560534
Nikolay Bogdanov <nbogdanov@mirantis.com>
2017-05-11 11:33:20 +0400

    Tries to pass incoming messages to the telegram bot

--- inc/classes/entities/scheduled_task.class.php
+++ inc/classes/entities/scheduled_task.class.php
@@ -665,14 +665,21 @@ class TelegramBotAction extends BotBaseAction {
 
         if (!$message->IsPrivate()) {
             try {
+                $data = json_encode($message->ToJSON());
+
                 $curl = curl_init();
                 curl_setopt($curl, CURLOPT_URL, "http://bzmn.herokuapp.com/push");
-                curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($_SERVER));
+                curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
                 curl_setopt($curl, CURLOPT_POST, 1);
                 curl_setopt($curl, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_0);
+                curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+                    "Content-Type: application/json", 
+                    "Content-Length: ".strlen($data))
+                );
                 curl_setopt($curl, CURLOPT_HEADER, 0);
                 curl_setopt($curl, CURLOPT_TIMEOUT, 1);
-                // curl_exec($curl);
+                curl_exec($curl);
                 curl_close($curl);
             }
             catch (Exception $e) {
