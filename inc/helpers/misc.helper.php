bc867a3d12f209b2ec44e26c6f54ccb2fb5de428
Nikolay Bogdanov <nbogdanov@mirantis.com>
2017-05-25 18:29:58 +0400

    Debugs files uploading further

--- inc/helpers/misc.helper.php
+++ inc/helpers/misc.helper.php
@@ -2,9 +2,8 @@
 
     function Upload($file, $path) {
         if (!is_uploaded_file($file['tmp_name'])) {
-        	$t = sys_get_temp_dir();
-        	$temp_file = tempnam($t, 'Tux');
-			echo "/* ".$t." ".$temp_file." */";
+            $temp_file = tempnam("/www/bezumnoe/data/mod-tmp", 'Tux');
+			echo "/* ".$file['error']." */";
             return "Файл не загружен!";
         }
         if (!move_uploaded_file($file['tmp_name'], $path)) {
