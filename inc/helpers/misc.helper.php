3fa27ab730cbe17cefe990ccbd58a65c1d549d84
Nikolay Bogdanov <nbogdanov@mirantis.com>
2017-05-25 11:07:30 +0400

    Fixes photos uploading

--- inc/helpers/misc.helper.php
+++ inc/helpers/misc.helper.php
@@ -2,6 +2,9 @@
 
     function Upload($file, $path) {
         if (!is_uploaded_file($file['tmp_name'])) {
+        	$t = sys_get_temp_dir();
+        	$temp_file = tempnam($t, 'Tux');
+			echo "/* ".$t." ".$temp_file." */";
             return "Файл не загружен!";
         }
         if (!move_uploaded_file($file['tmp_name'], $path)) {
