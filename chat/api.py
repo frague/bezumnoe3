fc2e789493f8909ff2f63bcca08c5bc3eb4a86a2
Nikolay Bogdanov <nbogdanov@mirantis.com>
2018-01-24 23:46:48 +0400

    Adds dummy api script

---
+++ chat/api.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+counter = 0
+
+@app.route('/')
+def increase():
+  global counter
+
+  counter += 1
+  return str(counter)
+
+if __name__ == '__main__':
+  app.run()
