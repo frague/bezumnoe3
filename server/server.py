1350f99b00db5c0db92c56ac5b780882d25c85c6
Nikolay Bogdanov <nbogdanov@mirantis.com>
2018-02-07 23:23:23 +0400

    First api blueprint

--- server/server.py
+++ server/server.py
@@ -2,10 +2,14 @@ from flask import Flask
 from models.user import User
 from shared.db import db
 
+from api.authenticate import authenticate
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://frague_mysql:+MU7qAqh@frague.mysql/frague_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+app.register_blueprint(authenticate)
 
 with app.app_context():
     query = User.query.options()
