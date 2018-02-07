6d4f53a4e34effe595eaede7e78b925704b5e2ad
Nikolay Bogdanov <nbogdanov@mirantis.com>
2018-02-07 23:38:39 +0400

    Makes flask app run after the script execution

--- server/server.py
+++ server/server.py
@@ -7,11 +7,17 @@ from api.authenticate import authenticate
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://frague_mysql:+MU7qAqh@frague.mysql/frague_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)
 
 app.register_blueprint(authenticate)
 
+db.init_app(app)
+
 with app.app_context():
-    query = User.query.options()
-    for user in query:
-        print user.login, user.id
+#    query = User.query.options()
+#    for user in query:
+#        print user.login, user.id
+	pass
+
+
+if __name__ == '__main__':
+    app.run(port=9000)
