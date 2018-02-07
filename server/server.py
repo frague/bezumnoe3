546369ec8a3757c08e7589d7441fd5212fde4ac2
Nikolay Bogdanov <nbogdanov@mirantis.com>
2018-02-08 01:00:06 +0400

    Fixes api url prefix

--- server/server.py
+++ server/server.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://frague_mysql:+MU7qAqh@frague.mysql/frague_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.add_url_rule('%sstatus' % api_prefix, view_func=StatusAPI.as_view('status'))
-app.add_url_rule('%sauthenticate' % api_prefix, view_func=AuthenticateAPI.as_view('authenticate'))
+app.add_url_rule('%s/status' % api_prefix, view_func=StatusAPI.as_view('status'))
+app.add_url_rule('%s/authenticate' % api_prefix, view_func=AuthenticateAPI.as_view('authenticate'))
 
 db.init_app(app)
 
