ee75778d1f7c6b2749b2c4c7d5691cd63e081e72
Nick Bogdanov (nic) <frague@mail.ru>
2018-02-08 12:57:25 +0300

    Fixes problem with mysql encoding

--- server/server.py
+++ server/server.py
@@ -1,3 +1,6 @@
+#!/usr/local/bin/python
+# coding: utf-8
+
 from flask import Flask
 from models.user import User
 from shared.db import db
@@ -9,6 +12,7 @@ from api.health import StatusAPI
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://frague_mysql:+MU7qAqh@frague.mysql/frague_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#app.config['SQLALCHEMY_ECHO'] = True
 
 app.add_url_rule('%s/status' % api_prefix, view_func=StatusAPI.as_view('status'))
 app.add_url_rule('%s/authenticate' % api_prefix, view_func=AuthenticateAPI.as_view('authenticate'))
@@ -16,10 +20,12 @@ app.add_url_rule('%s/authenticate' % api_prefix, view_func=AuthenticateAPI.as_vi
 db.init_app(app)
 
 with app.app_context():
-#    query = User.query.options()
-#    for user in query:
-#        print user.login, user.id
-	pass
+    db.engine.execute('SET NAMES utf8')
+    db.engine.execute('SET character_set_connection=utf8')
+
+    query = User.query.options()
+    for user in query:
+        print user.login, user.id
 
 
 if __name__ == '__main__':
