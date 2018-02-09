47e475adab1ab4a21461c302432f9faff796ca99
Nick Bogdanov (nic) <frague@mail.ru>
2018-02-09 10:07:18 +0300

    Validates user credentials in the db

--- server/api/authenticate.py
+++ server/api/authenticate.py
@@ -1,20 +1,21 @@
+import hashlib
 from flask import abort
 from webargs import fields as parse_fields
 from webargs.flaskparser import use_args
-from flask_restplus import Namespace, Resource
+from flask_restplus import Namespace, Resource, fields
 from models.user import User
 
 api = Namespace('authentication', description='User authentication')
 
-login_fields = api.model('', {
+login_fields = api.model('User', {
     'login': fields.String(required=True),
     'password': fields.String(required=True)
 })
 
-login_parse_fields = api.model('', {
+login_parse_fields = {
     'login': parse_fields.Str(required=True),
     'password': parse_fields.Str(required=True)
-})
+}
 
 @api.route('/')
 class AuthenticateAPI(Resource):
@@ -27,6 +28,8 @@ class AuthenticateAPI(Resource):
         password = args['password']
         if login is None or password is None:
             return abort(404)
+	password = hashlib.md5(password).hexdigest()
+        print('%s:%s' % (login, password))
         logged_user = User.query.filter_by(login=login, password=password).first()
         if logged_user is None:
             return abort(400)
