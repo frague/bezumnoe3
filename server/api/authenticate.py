aae268fc0f7b4feb0e68c8da7b4e2bc11af5a16e
Nick Bogdanov <nbogdanov@griddynamics.com>
2018-02-09 15:18:45 +0400

    Attempts to store session

--- server/api/authenticate.py
+++ server/api/authenticate.py
@@ -1,7 +1,7 @@
 import hashlib
 import uuid
 from datetime import datetime
-from flask import abort
+from flask import abort, session
 from webargs import fields as parse_fields
 from webargs.flaskparser import use_args
 from flask_restplus import Namespace, Resource, fields
@@ -20,26 +20,21 @@ post_args = {
     'password': parse_fields.Str(required=True)
 }
 
-cookies_args = {
-    'bzmn': parse_fields.Str(required=True)
-}
-
 @api.route('/')
 class AuthenticateAPI(Resource):
     @api.expect(login_fields)
     @api.response(200, 'Success')
     @api.response(403, 'Not authenticated')
     @use_args(post_args, locations=('form', ))
-    @use_args(cookies_args, locations=('cookies', ))
     def post(self, form, cookies):
         login = form['login']
         password = form['password']
-        session = cookies['bzmn']
+        bzmn = session['bzmn']
 
         if bzmn is not None:
             # Authentication via session token
-            print('Auth via session: %s' % (session, ))
-            logged_user = User.query.filter_by(session=session).first_or_404()
+            print('Auth via session: %s' % (bzmn, ))
+            logged_user = User.query.filter_by(session=bzmn).first_or_404()
 
         elif login is not None and password is not None:
             # Authenticate via login & password
@@ -47,6 +42,7 @@ class AuthenticateAPI(Resource):
             print('Auth via login & password: %s:%s' % (login, password))
             logged_user = User.query.filter_by(login=login, password=password).first_or_404()
             logged_user.session = uuid.uuid4().hex
+            session['bzmn'] = logged_user.session
 
         else:
             # Not found
