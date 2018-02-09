4b0858e439f9b6ff43d93d640e57c92e193fd955
Nick Bogdanov <nbogdanov@griddynamics.com>
2018-02-09 12:30:13 +0400

    Authenticates user via key in cookies

--- server/api/authenticate.py
+++ server/api/authenticate.py
@@ -1,5 +1,6 @@
 import hashlib
 import uuid
+from datetime import datetime
 from flask import abort
 from webargs import fields as parse_fields
 from webargs.flaskparser import use_args
@@ -14,9 +15,13 @@ login_fields = api.model('User', {
     'password': fields.String(required=True)
 })
 
-login_parse_fields = {
+post_args = {
     'login': parse_fields.Str(required=True),
     'password': parse_fields.Str(required=True)
+}
+
+cookies_args = {
+    'bzmn': parse_fields.Str(required=True)
 }
 
 @api.route('/')
@@ -24,18 +29,30 @@ class AuthenticateAPI(Resource):
     @api.expect(login_fields)
     @api.response(200, 'Success')
     @api.response(403, 'Not authenticated')
-    @use_args(login_parse_fields)
-    def post(self, args):
-        login = args['login']
-        password = args['password']
-        if login is None or password is None:
-            return abort(404)
+    @use_args(post_args, locations=('form', ))
+    @use_args(cookies_args, locations=('cookies', ))
+    def post(self, form, cookies):
+        login = form['login']
+        password = form['password']
+        session = cookies['bzmn']
 
-	password = hashlib.md5(password).hexdigest()
-        print('%s:%s' % (login, password))
-        logged_user = User.query.filter_by(login=login, password=password).first_or_404()
+        if bzmn is not None:
+            # Authentication via session token
+            print('Auth via session: %s' % (session, ))
+            logged_user = User.query.filter_by(session=session).first_or_404()
 
-        logged_user.session = uuid.uuid4().hex
+        elif login is not None and password is not None:
+            # Authenticate via login & password
+            password = hashlib.md5(password).hexdigest()
+            print('Auth via login & password: %s:%s' % (login, password))
+            logged_user = User.query.filter_by(login=login, password=password).first_or_404()
+            logged_user.session = uuid.uuid4().hex
+
+        else:
+            # Not found
+            return abort(400)
+
+        logged_user.session_pong = datetime.datetime.now().isoformat(' ')
         db.session.add(logged_user)
         db.session.commit()
 
