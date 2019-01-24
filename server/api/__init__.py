1d45618e1834a784f409f096b6727371c55fdc3e
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2019-01-24 19:57:10 +0400

    Removes previously submitted monkey patch

--- server/api/__init__.py
+++ server/api/__init__.py
@@ -5,13 +5,6 @@ from health import api as ns_health
 from user import api as ns_user
 from authenticate import api as ns_auth
 
-@Property
-def specs_url(self):
-    """Monkey patch for HTTPS"""
-    return url_for(self.endpoint('specs'), _external=True, _scheme='https')
-
-Api.specs_url = specs_url
-
 version = '1.0'
 blueprint = Blueprint('api', __name__, url_prefix='/api/%s' % version)
 api = Api(blueprint, version=version, title='API', description='bezumnoe.ru public API', doc='/doc/')
