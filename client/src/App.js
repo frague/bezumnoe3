6dbf10a6a449682b2c9806c3fbf73b0bd1503a3f
Nick Bogdanov <nikolay.bogdanov@gmail.com>
2019-08-12 23:31:46 +0400

    Creates initial react bootstrap

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header>bzmn4</header>
+      <section>
+      </section>
+    </div>
+  );
+}
+
+export default App;
--- references.php
+++
@@ -1,5 +0,0 @@
-<?
-    require_once "server_references.php";
-
-?>
-<script src="/js/<? include $root."js/handler.php" ?>.javascript"></script>
